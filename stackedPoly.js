5dff1a9e861b18ec1db420dbb6460b22603cb066
Jacob Richards <jacob.richards33@gmail.com>
2016-07-07 00:42:35 -0700

    added n deleted

--- stackedPoly.js
+++
@@ -1,9 +0,0 @@
-timbre.bpm = 180;
-
-var synth = T("OscGen", {wave:"tri", mul:0.2});
-
-T("interval", {interval:"L4", timeout:"5sec"}, function() {
-    synth.noteOn(69, 80);
-}).on("ended", function() {
-    this.stop();
-}).set({buddies:synth}).start();
