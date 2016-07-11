51e5d781b65ac65eaf95e9e180e110e5b400f7b6
euglazer <eugenestapler@gmail.com>
2016-07-10 20:23:08 -0400

    remove whitespace

--- js/index.js
+++ js/index.js
@@ -7,13 +7,9 @@ $('#stop').click(location.reload)
 
 $('#form').submit(function (e) {
   e.preventDefault()
-
   $('#start').hide()
   $('#stop').show()
-
   var opts = formToObject(this)
-
   timbrePoly(opts)
-
   $('#description').html(opts.levels + ' levels of ' + opts.A + ':' + opts.B + ' polyrhythm  at ' + opts.bpm + ' BPM')
 })
