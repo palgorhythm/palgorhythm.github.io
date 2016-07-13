266adb8e581903c2103297c9753308dc9d922592
euglazer <eugenestapler@gmail.com>
2016-07-13 19:53:33 -0400

    dotarrays shifting for each interval hit

--- js/interval.js
+++ js/interval.js
@@ -9,7 +9,8 @@ var env  = T('perc', {a: 50, r: 2500})
 class Interval {
   constructor (opts) {
     this.opts = opts
-    this.$dots = this._generateDots(opts.id)
+    this.$dots = this._generateDots()
+    this.counter = 0
   }
 
   start () {
@@ -17,6 +18,9 @@ class Interval {
 
     var inter = T('interval', {interval: this.opts.interval}, () => {
       sin.noteOnWithFreq(this.opts.freq, 80)
+      var dataIndex = this.counter % this.$dots.length
+      this._showDot(dataIndex)
+      this.counter++
     }).set({buddies: sin})
 
     inter.start()
@@ -24,15 +28,15 @@ class Interval {
 
   // 'private'
 
-  _generateDots (id) {
+  _generateDots () {
     var size = 10, startRadius = 140, radiusInterval = 20, numOfDotsPerFreq = 8
     var numOfFreqs = this.opts.levels + 1, degreeInterval = 2 * Math.PI / numOfFreqs
     var centerCoords = { x: $body.width() / 2, y: $body.height() / 2 }
 
     return range(numOfDotsPerFreq).map((i) => {
       var coords = {
-        x: (startRadius + i * radiusInterval) * Math.cos(degreeInterval * id) + centerCoords.x - size / 2,
-        y: (startRadius + i * radiusInterval) * Math.sin(degreeInterval * id) + centerCoords.y - size / 2
+        x: (startRadius + i * radiusInterval) * Math.cos(degreeInterval * this.opts.id) + centerCoords.x - size / 2,
+        y: (startRadius + i * radiusInterval) * Math.sin(degreeInterval * this.opts.id) + centerCoords.y - size / 2
       }
 
       var $dot = $('<div class="dot"></div>').css({
@@ -40,10 +44,15 @@ class Interval {
         right: coords.x,
         width: size,
         height: size
-      })
+      }).attr('data-index', i)
       $('body').append($dot)
       return $dot.css({ backgroundColor: colors[this.opts.id] })
     })
+  }
+
+  _showDot (i) {
+    this.$dots.forEach(($dot) => ($dot.hide()))
+    this.$dots[i].show()
   }
 }
 
