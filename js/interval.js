ae39b97ec545d53591f63a1376f0bcf5035af191
euglazer <eugenestapler@gmail.com>
2016-07-13 19:40:47 -0400

    rendering multiple dots per freq

--- js/interval.js
+++ js/interval.js
@@ -1,5 +1,6 @@
 import T from 'timbre'
 import $ from 'jquery'
+import range from 'lodash.range'
 
 var colors = ['#F5BF16', '#5CBAEB', '#FF385C', '#323B4A', '#59EAE6', '#BDD4DE', '#FF530D', '#3F5765']
 var $body = $('body')
@@ -8,7 +9,7 @@ var env  = T('perc', {a: 50, r: 2500})
 class Interval {
   constructor (opts) {
     this.opts = opts
-    this.$dot = this._generateDot(opts.id)
+    this.$dots = this._generateDots(opts.id)
   }
 
   start () {
@@ -23,17 +24,26 @@ class Interval {
 
   // 'private'
 
-  _generateDot (id) {
-    var radius = 200, numOfDots = this.opts.levels + 1, degreeInterval = 2 * Math.PI / numOfDots
+  _generateDots (id) {
+    var size = 10, startRadius = 140, radiusInterval = 20, numOfDotsPerFreq = 8
+    var numOfFreqs = this.opts.levels + 1, degreeInterval = 2 * Math.PI / numOfFreqs
     var centerCoords = { x: $body.width() / 2, y: $body.height() / 2 }
-    var coords = {
-      x: 200 * Math.cos(degreeInterval * id) + centerCoords.x,
-      y: 200 * Math.sin(degreeInterval * id) + centerCoords.y
-    }
 
-    var $dot = $('<div class="dot"></div>').css({ top: coords.y, right: coords.x, })
-    $('body').append($dot)
-    return $dot.css({ backgroundColor: colors[this.opts.id] })
+    return range(numOfDotsPerFreq).map((i) => {
+      var coords = {
+        x: (startRadius + i * radiusInterval) * Math.cos(degreeInterval * id) + centerCoords.x - size / 2,
+        y: (startRadius + i * radiusInterval) * Math.sin(degreeInterval * id) + centerCoords.y - size / 2
+      }
+
+      var $dot = $('<div class="dot"></div>').css({
+        top: coords.y,
+        right: coords.x,
+        width: size,
+        height: size
+      })
+      $('body').append($dot)
+      return $dot.css({ backgroundColor: colors[this.opts.id] })
+    })
   }
 }
 
